7d549eb0e19baab0947f092470ec85d1ab177477
Igor Smolyar <igors@cs.technion.ac.il>
2021-03-26 08:07:24 +0300

    add build-kernel.sh script

---
+++ build-kernel.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+kerneldir=linux-kernel
+kerneltar=linux-kernel-5.0.0-16.17
+patch=cinterrupts.patch
+config=config-file
+
+pushd $kerneldir
+echo -n "Extracting kernel...."
+tar -zxf $kerneltar.tgz 
+echo -n "Done"
+
+pushd $kerneltar
+echo "Applying patch"
+patch -p1 < ../$patch
+cp ../$config .config
+
+echo "Start compilation"
+make oldconfig
+make bzImage -j 28
+make modules -j 28
+sudo make modules_install -j 28
+sudo make install
+
+popd
+popd
+
+echo "Done! Set proper grub entry and reboot."

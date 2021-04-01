c72f687cda22af3b8212135971f48d5ad19515b4
Amy Tai <amy.tai.2009@gmail.com>
2021-04-01 23:06:40 +0300

    Set scaling_freq in run scripts

--- fig16/all.sh
+++ fig16/all.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+sudo sh -c "echo 2000000 > /sys/devices/system/cpu/cpu9/cpufreq/scaling_min_freq"
+sudo sh -c "echo 2000000 > /sys/devices/system/cpu/cpu5/cpufreq/scaling_min_freq"
+sudo sh -c "echo 2000000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq"
+sudo sh -c "echo 2000000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq"
+
 ./load.sh 2 4
 
 rm -rf results/
--- tab3/fillbatch.sh
+++ tab3/fillbatch.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+sudo sh -c "echo 2000000 > /sys/devices/system/cpu/cpu9/cpufreq/scaling_min_freq"
+sudo sh -c "echo 2000000 > /sys/devices/system/cpu/cpu5/cpufreq/scaling_min_freq"
+sudo sh -c "echo 2000000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq"
+sudo sh -c "echo 2000000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq"
 
 rocksdbdir=../rocksdb/rocksdb-6.4.6
 

6dace56cc3245ca5ed49df939ff42e7163b9d7cc
Amy Tai <amy.tai.2009@gmail.com>
2021-04-01 18:42:10 +0300

    Update scripts to include stdev

--- fig16/parse_99.sh
+++ fig16/parse_99.sh
@@ -19,7 +19,10 @@ do
 	grep "99p" results/*_raw_N_0_0_${run}.out | awk '{print $4}' >> default.out.${run}.tmp
 done
 
-paste default.out.0.tmp default.out.1.tmp default.out.2.tmp default.out.3.tmp default.out.4.tmp | awk '{print ($1+$2+$4+$4+$5)/5}' >> default.out.tmp
+paste default.out.0.tmp default.out.1.tmp default.out.2.tmp default.out.3.tmp default.out.4.tmp | awk '{print ($1+$2+$3+$4+$5)/5}' > mean.tmp
+paste default.out.0.tmp default.out.1.tmp default.out.2.tmp default.out.3.tmp default.out.4.tmp mean.tmp | awk '{print "+/-",  sqrt((($1-$6)*($1-$6)+($2-$6)*($2-$6)+($3-$6)*($3-$6)+($4-$6)*($4-$6)+($5-$6)*($5-$6))/5)}' > stdev.tmp
+
+paste -d ' ' mean.tmp stdev.tmp >> default.out.tmp
 
 echo "-------------------" >> cint.out.tmp
 echo "cint:" >> cint.out.tmp
@@ -29,7 +32,10 @@ do
 	grep "99p" results/*_raw_Y_32_15_${run}.out | awk '{print $4}' >> cint.out.${run}.tmp
 done
 
-paste cint.out.0.tmp cint.out.1.tmp cint.out.2.tmp cint.out.3.tmp cint.out.4.tmp | awk '{print ($1+$2+$4+$4+$5)/5}' >> cint.out.tmp
+paste cint.out.0.tmp cint.out.1.tmp cint.out.2.tmp cint.out.3.tmp cint.out.4.tmp | awk '{print ($1+$2+$3+$4+$5)/5}' > mean.tmp
+paste cint.out.0.tmp cint.out.1.tmp cint.out.2.tmp cint.out.3.tmp cint.out.4.tmp mean.tmp | awk '{print "+/-",  sqrt((($1-$6)*($1-$6)+($2-$6)*($2-$6)+($3-$6)*($3-$6)+($4-$6)*($4-$6)+($5-$6)*($5-$6))/5)}' > stdev.tmp
+
+paste -d ' ' mean.tmp stdev.tmp >> cint.out.tmp
 
 echo "-------------------" >> adaptive.out.tmp
 echo "adaptive:" >> adaptive.out.tmp
@@ -39,7 +45,10 @@ do
 	grep "99p" results/*_raw_N_32_15_${run}.out | awk '{print $4}' >> adaptive.out.${run}.tmp
 done
 
-paste adaptive.out.0.tmp adaptive.out.1.tmp adaptive.out.2.tmp adaptive.out.3.tmp adaptive.out.4.tmp | awk '{print ($1+$2+$4+$4+$5)/5}' >> adaptive.out.tmp
+paste adaptive.out.0.tmp adaptive.out.1.tmp adaptive.out.2.tmp adaptive.out.3.tmp adaptive.out.4.tmp | awk '{print ($1+$2+$3+$4+$5)/5}' > mean.tmp
+paste adaptive.out.0.tmp adaptive.out.1.tmp adaptive.out.2.tmp adaptive.out.3.tmp adaptive.out.4.tmp mean.tmp | awk '{print "+/-",  sqrt((($1-$6)*($1-$6)+($2-$6)*($2-$6)+($3-$6)*($3-$6)+($4-$6)*($4-$6)+($5-$6)*($5-$6))/5)}' > stdev.tmp
+
+paste -d ' ' mean.tmp stdev.tmp >> adaptive.out.tmp
 
 paste labels.tmp default.out.tmp cint.out.tmp adaptive.out.tmp | column -s $'\t' -t > 99lat.out
 echo "99p latency:"
--- fig16/parse_throughput.sh
+++ fig16/parse_throughput.sh
@@ -19,8 +19,12 @@ do
 	grep "req/s" results/*_raw_N_0_0_${run}.out | awk '{print $10}' | awk -F'(' '{print $2}' >> default.thru.out.${run}.tmp
 done
 
-paste default.thru.out.0.tmp default.thru.out.1.tmp default.thru.out.2.tmp default.thru.out.3.tmp default.thru.out.4.tmp | awk '{print ($1+$2+$4+$4+$5)/5}' >> default.thru.out.tmp
+paste default.thru.out.0.tmp default.thru.out.1.tmp default.thru.out.2.tmp default.thru.out.3.tmp default.thru.out.4.tmp | awk '{print ($1+$2+$3+$4+$5)/5}' > mean.tmp
+paste default.thru.out.0.tmp default.thru.out.1.tmp default.thru.out.2.tmp default.thru.out.3.tmp default.thru.out.4.tmp mean.tmp | awk '{print "+/-",  sqrt((($1-$6)*($1-$6)+($2-$6)*($2-$6)+($3-$6)*($3-$6)+($4-$6)*($4-$6)+($5-$6)*($5-$6))/5)}' > stdev.tmp
 
+paste -d ' ' mean.tmp stdev.tmp >> default.thru.out.tmp
+
+#cint file
 echo "-------------------" >> cint.thru.out.tmp
 echo "cint:" >> cint.thru.out.tmp
 echo "-------------------" >> cint.thru.out.tmp
@@ -29,7 +33,10 @@ do
 	grep "req/s" results/*_raw_Y_32_15_${run}.out | awk '{print $10}' | awk -F'(' '{print $2}' >> cint.thru.out.${run}.tmp
 done
 
-paste cint.thru.out.0.tmp cint.thru.out.1.tmp cint.thru.out.2.tmp cint.thru.out.3.tmp cint.thru.out.4.tmp | awk '{print ($1+$2+$4+$4+$5)/5}' >> cint.thru.out.tmp
+paste cint.thru.out.0.tmp cint.thru.out.1.tmp cint.thru.out.2.tmp cint.thru.out.3.tmp cint.thru.out.4.tmp | awk '{print ($1+$2+$3+$4+$5)/5}' > mean.tmp
+paste cint.thru.out.0.tmp cint.thru.out.1.tmp cint.thru.out.2.tmp cint.thru.out.3.tmp cint.thru.out.4.tmp mean.tmp | awk '{print "+/-",  sqrt((($1-$6)*($1-$6)+($2-$6)*($2-$6)+($3-$6)*($3-$6)+($4-$6)*($4-$6)+($5-$6)*($5-$6))/5)}' > stdev.tmp
+
+paste -d ' ' mean.tmp stdev.tmp >> cint.thru.out.tmp
 
 echo "-------------------" >> adaptive.thru.out.tmp
 echo "adaptive:" >> adaptive.thru.out.tmp
@@ -39,7 +46,10 @@ do
 	grep "req/s" results/*_raw_N_32_15_${run}.out | awk '{print $10}' | awk -F'(' '{print $2}' >> adaptive.thru.out.${run}.tmp
 done
 
-paste adaptive.thru.out.0.tmp adaptive.thru.out.1.tmp adaptive.thru.out.2.tmp adaptive.thru.out.3.tmp adaptive.thru.out.4.tmp | awk '{print ($1+$2+$4+$4+$5)/5}' >> adaptive.thru.out.tmp
+paste adaptive.thru.out.0.tmp adaptive.thru.out.1.tmp adaptive.thru.out.2.tmp adaptive.thru.out.3.tmp adaptive.thru.out.4.tmp | awk '{print ($1+$2+$3+$4+$5)/5}' > mean.tmp
+paste adaptive.thru.out.0.tmp adaptive.thru.out.1.tmp adaptive.thru.out.2.tmp adaptive.thru.out.3.tmp adaptive.thru.out.4.tmp mean.tmp | awk '{print "+/-",  sqrt((($1-$6)*($1-$6)+($2-$6)*($2-$6)+($3-$6)*($3-$6)+($4-$6)*($4-$6)+($5-$6)*($5-$6))/5)}' > stdev.tmp
+
+paste -d ' ' mean.tmp stdev.tmp >> adaptive.thru.out.tmp
 
 paste labels.tmp default.thru.out.tmp cint.thru.out.tmp adaptive.thru.out.tmp | column -s $'\t' -t > thru.out
 echo "Throughput:"

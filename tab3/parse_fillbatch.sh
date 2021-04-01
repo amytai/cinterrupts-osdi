d8943d43a26d47115ca08a7a32af796720552757
Amy Tai <amy.tai.2009@gmail.com>
2021-04-01 05:50:20 +0300

    Add stdev

--- tab3/parse_fillbatch.sh
+++ tab3/parse_fillbatch.sh
@@ -15,7 +15,9 @@ echo "99% latency" >> 99.out.tmp
 echo "------------" >> 99.out.tmp
 for name in unmodified_app_cint default adaptive cint
 do
-grep "rocksdb.db.write.micros" results/fillbatch_${name}_* | awk '{print $10}' | awk '{sum += $1} END {print sum/5}' >> 99.out.tmp
+mean=`grep "rocksdb.db.write.micros" results/fillbatch_${name}_* | awk '{print $10}' | awk '{sum += $1} END {print sum/5}'`
+stdev=`grep "rocksdb.db.write.micros" results/fillbatch_${name}_* | awk '{print $10}' | awk -v mean=$mean '{sum += ($1-mean)*($1-mean)} END {print sqrt(sum/5)}'`
+echo $mean "+/-" $stdev >> 99.out.tmp
 done
 
 echo "------------" >> avg.out.tmp
@@ -23,7 +25,9 @@ echo "avg latency" >> avg.out.tmp
 echo "------------" >> avg.out.tmp
 for name in unmodified_app_cint default adaptive cint
 do
-grep "rocksdb.db.write.micros" results/fillbatch_${name}_* | awk '{print $4}' | awk '{sum += $1} END {print sum/5}' >> avg.out.tmp
+mean=`grep "rocksdb.db.write.micros" results/fillbatch_${name}_* | awk '{print $4}' | awk '{sum += $1} END {print sum/5}'`
+stdev=`grep "rocksdb.db.write.micros" results/fillbatch_${name}_* | awk '{print $4}' | awk -v mean=$mean '{sum += ($1-mean)*($1-mean)} END {print sqrt(sum/5)}'`
+echo $mean "+/-" $stdev >> avg.out.tmp
 done
 
 
@@ -32,7 +36,9 @@ echo "throughput" >> thru.out.tmp
 echo "------------" >> thru.out.tmp
 for name in unmodified_app_cint default adaptive cint
 do
-grep "ops/sec;" results/fillbatch_${name}_* | awk '{print $5}' | awk '{sum += $1} END {print sum/5}' >> thru.out.tmp
+mean=`grep "ops/sec;" results/fillbatch_${name}_* | awk '{print $5}' | awk '{sum += $1} END {print sum/5}'`
+stdev=`grep "ops/sec;" results/fillbatch_${name}_* | awk '{print $5}' | awk -v mean=$mean '{sum += ($1-mean)*($1-mean)} END {print sqrt(sum/5)}'`
+echo $mean "+/-" $stdev >> thru.out.tmp
 done
 
 paste labels.tmp thru.out.tmp avg.out.tmp 99.out.tmp  | column -s $'\t' -t > all.out
